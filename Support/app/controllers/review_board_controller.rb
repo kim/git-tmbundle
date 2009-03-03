699f0e325d26d9d0db6b4a8dbbb0844320b88493
Horaci Cuevas <horaci.cuevas@xing.com>
2009-03-03 19:17:00 +0100

    Added JSON support bundled and option to reconfigure settings

--- Support/app/controllers/review_board_controller.rb
+++ Support/app/controllers/review_board_controller.rb
@@ -1,17 +1,22 @@
 require 'uri'
 require ENV['TM_SUPPORT_PATH'] + '/lib/tm/process.rb'
 require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
+require 'strscan'
 
-require 'rubygems'
-require 'json'
-require 'net/http'
+%w[json/version json/pure/parser json/pure/generator json/common json/pure json].each do |filename|
+  require File.dirname(__FILE__) + "/../../lib/#{filename}.rb"
+end
 
 class ReviewBoardController < ApplicationController
 
+  REVIEWBOARD_COOKIE_NAME = "~/.reviewboard.cookie"
+
   class LoginRequiredError < RuntimeError; end
+  class UpstreamRequiredError < RuntimeError; end
   class RepositoryIdRequiredError < RuntimeError; end
 
   def index
+
     current_branch = git.branch.current.name
 
     # try to get reviewboard url from config, otherwise ask user
@@ -21,30 +26,17 @@ class ReviewBoardController < ApplicationController
       @reviewboard_url = URI.parse(url)
       @reviewboard_url = @reviewboard_url.to_s.chomp!("/")
     rescue URI::InvalidURIError
-      url = TextMate::UI.request_string(:title => "Which Reviewboard?", :prompt => "Reviewboard URL:").strip
-      unless url.empty?
-        git.command("config", "reviewboard.url", url)
-        retry
-      else
-        puts "<p>So you don't wanna tell me which URL?</p>"
-        return
-      end
+      url = ask_config( "reviewboard.url", "Which Reviewboard?", "Reviewboard URL:" ) || return
+      retry
     end
 
     # try to get the upstream from config, otherwise ask user (default: master)
     begin
       @upstream ||= git.command("config", "--get", "reviewboard.upstream").strip
-      raise URI::InvalidURIError, "Empty upstream.." if @upstream.empty?
-    rescue URI::InvalidURIError
-      @upstream = TextMate::UI.request_string(:title => "Which Upstream to diff?", :default => @upstream.empty? ? "master" : @upstream, :prompt => "Upstream branch:").strip
-      unless @upstream.empty?
-        # Todo: Check that upstream actually exists?
-        git.command("config", "reviewboard.upstream", @upstream)
-        retry
-      else
-        puts "<p>Upstream can't be empty, aborting...</p>"
-        return
-      end
+      raise ReviewBoardController::UpstreamRequiredError, "Empty upstream.." if @upstream.empty?
+    rescue ReviewBoardController::UpstreamRequiredError
+      @upstream = ask_config( "reviewboard.upstream", "Which Upstream to diff?", "Upstream branch:" ) || return
+      retry
     end
 
     opts = {}
@@ -53,23 +45,11 @@ class ReviewBoardController < ApplicationController
       raise ReviewBoardController::RepositoryIdRequiredError, "No repository id in config" if @repository_id.empty?
       opts[:repository_id] = @repository_id.to_s
     rescue ReviewBoardController::RepositoryIdRequiredError
-      # Get available repositories
-      items = (get_available_repositories || []).map { |x| "##{x['id']}: #{x['name']}" }
-      selection = TextMate::UI.request_item(:items => items,  :title => "Choose to which repository you want to publish?", :prompt => "Repository:").strip
-      if selection =~ /^#(\d*):.*/
-        @repository_id = $1.to_s
-        git.command("config", "--add", "reviewboard.repositoryid", @repository_id )
-        retry
-      else
-        puts "<p>No repository selected, aborting ...</p>"
-        puts "<p>Please use git config --add reviewboard.repository_id|reviewboard.repository_path &lt;ID|path&gt; if you don't get a list of available repositories"
-        return
-      end
+      @repository_id = ask_repositoryid() || return
+      retry
     end
 
     response = create_new_request(opts)
-
-    git.command("config", "reviewboard.repository_id", response["review_request"]["repository"]["id"])
     review_request_id = response["review_request"]["id"]
     upload_diff(review_request_id, git.command("diff", "--no-color", "--no-prefix", @upstream))
 
@@ -77,13 +57,48 @@ class ReviewBoardController < ApplicationController
     puts "Review sent. <a href=\"#{@reviewboard_url}/r/#{review_request_id}\">Click here to edit your review request</a>"
   end
 
+  def reconfigure
+    @reviewboard_url = ask_config_value( "reviewboard.url", "Which Reviewboard?", "Reviewboard URL:" )
+    ask_user_and_password_and_login()
+    ask_config_value( "reviewboard.upstream", "Which Upstream to diff?", "Upstream branch:" ) || return
+    ask_repositoryid() || return
+    puts "<p>Configuration updated!</p>"
+  end
+
   private
+
+    def ask_config_value( value, title, label )
+      url = git.command("config", "--get", value ).strip
+      url = TextMate::UI.request_string(:title => title, :default => url, :prompt => label ).strip
+      unless url.empty?
+        git.command("config", value, url)
+        return url
+      else
+        puts "<p>#{value} can't be empty, aborting...</p>"
+        return nil
+      end
+    end
+
+    def ask_repositoryid( store=false, read=true )
+      # Get available repositories
+      items = (get_available_repositories || []).map { |x| "##{x['id']}: #{x['name']}" }
+      selection = TextMate::UI.request_item(:items => items, :title => "Choose to which repository you want to publish?", :prompt => "Repository:").strip
+      if selection =~ /^#(\d*):.*/
+        repository_id = $1.to_s
+        git.command("config", "--add", "reviewboard.repositoryid", repository_id )
+        return repository_id
+      else
+        puts "<p>No repository selected, aborting ...</p>"
+        return nil
+      end
+    end
 
     def ask_user_and_password_and_login
       username = TextMate::UI.request_string(:title => "Login required", :prompt => "Enter username:")
       password = TextMate::UI.request_secure_string(:title => "Login required", :prompt => "Enter password:")
 
       raise "No username or password given, aborting..." if username.empty? and password.empty?
+      File.delete( REVIEWBOARD_COOKIE_NAME ) if File.exists?( REVIEWBOARD_COOKIE_NAME )
       login(username, password)
     end
 
@@ -123,11 +138,12 @@ class ReviewBoardController < ApplicationController
     def api_post(path, params, files = {}, headers = {})
 
       begin
+        #TODO: Possible security issue, tmpfiles should be random and shall verify doesn't exist before
         files.keys.each do |key|
           File.open("/tmp/#{files[key][:filename]}", "wb") {|f| f.puts files[key][:content] }
         end
 
-        cmd = "curl -v --cookie ~/.reviewboard.cookie --cookie-jar ~/.reviewboard.cookie"
+        cmd = "curl -v --cookie #{REVIEWBOARD_COOKIE_NAME} --cookie-jar #{REVIEWBOARD_COOKIE_NAME}"
         params.each do |name, value|
           name = name.to_s
           value = value.to_s
@@ -135,12 +151,8 @@ class ReviewBoardController < ApplicationController
         end
 
         files.keys.each {|key| cmd << " --form \"#{key}=@/tmp/#{files[key][:filename]}\" "}
-
         cmd << " #{@reviewboard_url}/#{path}"
-
-#       puts "<p>cmd was #{cmd}</p>"
         response = TextMate::Process.run(cmd)
-#       puts "<p>response was #{response}</p>"
         response = JSON.parse(response[0])
         raise ReviewBoardController::LoginRequiredError, "Require login" if response_requires_login( response, true )
         return response
